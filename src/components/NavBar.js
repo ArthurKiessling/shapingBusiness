4146bea038f9042612b74d253d5b41bddfe23e1b
ArthurKiessling <arthur.kiessling@prime-force.com>
2024-01-31 09:08:47 +0100

    added some Files

---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as Hamburger } from '../images/menu_icon.svg'
+import './navbar.css'
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        <p>Connecting People - Shaping Business</p>
+        </div>
+        <div  className="menu-icon" onClick={handleShowNavbar}>
+        <Hamburger />
+        </div>
+       <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/beratung">Beratung</NavLink>
+            </li>
+            <li>
+              <NavLink to="/uebermich">Über mich</NavLink>
+            </li>
+            <li>
+              <NavLink to="/philosopie">Philosophie</NavLink>
+            </li>
+            <li>
+              <NavLink to="/kontakt">Kontakt</NavLink>
+            </li>
+            <li>
+              <NavLink to="/kontakt">de | en</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
