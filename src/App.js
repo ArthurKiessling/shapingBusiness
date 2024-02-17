fcd4e1a0589b14ec30786b2b58ca42e25596f9d4
ArthurKiessling <arthur.kiessling@prime-force.com>
2024-02-17 16:06:57 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     
     <ThemeProvider>
       <div className="flex flex-col min-h-screen">
-        <BrowserRouter basename="/shapingBusiness">
+        <BrowserRouter basename="/">
           <Navbar />
           <Routes>
             <Route path="/" element={<HomePage/>}/>
